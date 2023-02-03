395d0371117db255cc512dd1711ba8014a932a4d
mehrbanoo ebrahimzade <mehrbanooebrahimzade@gmail.com>
2023-02-03 21:33:53 +0330

    add api controller

--- RedisHSet/RedisHSet/Controllers/UsersController.cs
+++ RedisHSet/RedisHSet/Controllers/UsersController.cs
@@ -18,21 +18,37 @@ namespace RedisHSet.Controllers
             _userService = userService;
         }
 
-        [HttpPost]
+        [HttpPost("{count}")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
-        public async Task<ActionResult> SetHSet()
+        public async Task<ActionResult> SetHSet(int count)
         {
             _logger.LogInformation("Set data");
-            await _userService.Set();
+            await _userService.SetData(count);
             return Ok();
         }
 
         [HttpGet]
         [ProducesResponseType((int)HttpStatusCode.OK)]
-        public async Task<ActionResult<List<User>>> GetRandomUsers()
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            _logger.LogInformation("Get All Users");
+            return Ok(await _userService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<List<User>>> GetUserById(int id)
+        {
+            _logger.LogInformation("Get User By Id");
+            return Ok(await _userService.GetById(id));
+        }
+
+        [HttpGet("[action]/{count}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<List<User>>> GetRandomUsers(int count)
         {
             _logger.LogInformation("Get randomly");
-            return Ok(await _userService.Get());
+            return Ok(await _userService.GetRandomly(count));
         }
 
     }
