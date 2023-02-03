67aca1971c4d6b5a5dd9a86b6c39d09b6cd78a09
mehrbanoo ebrahimzade <mehrbanooebrahimzade@gmail.com>
2023-02-03 21:33:33 +0330

    add services

--- RedisHSet/RedisHSet/Services/IUserService.cs
+++ RedisHSet/RedisHSet/Services/IUserService.cs
@@ -4,7 +4,9 @@ namespace RedisHSet.Services
 {
     public interface IUserService
     {
-        public Task Set();
-        public Task<List<User>> Get();
+        public Task SetData(int count);
+        public Task<List<User>> GetAll();
+        public Task<User> GetById(int id);
+        public Task<List<User>> GetRandomly(int count);
     }
 }
--- RedisHSet/RedisHSet/Services/UserService.cs
+++ RedisHSet/RedisHSet/Services/UserService.cs
@@ -1,24 +1,47 @@
 ﻿using RedisHSet.Entities;
+using RedisHSet.Repository;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace RedisHSet.Services
 {
     public class UserService : IUserService
     {
-        private readonly  ILogger<UserService> _logger;
+        private readonly IUserRepository _userRepository;
+        private readonly IConnectionMultiplexer _redis;
 
-        public UserService(ILogger<UserService> logger)
+        public UserService(IUserRepository userRepository, IConnectionMultiplexer redis)
         {
-            _logger = logger;
+            _userRepository = userRepository;
+            _redis = redis;
         }
 
-        public async Task Set()
+        public async Task SetData(int count)
         {
-           
+            var users = _userRepository.CreateUsers(count);
+
+            foreach (var user in users)
+            {
+                await _redis.GetDatabase().HashSetAsync("user", user.Id, JsonSerializer.Serialize(user));
+            }
         }
 
-        public async Task<List<User>> Get()
+        public async Task<List<User>> GetAll()
         {
-            return new List<User>();
+            var usersHash = await _redis.GetDatabase().HashGetAllAsync("user");
+            return usersHash.Select(userH => JsonSerializer.Deserialize<User>(userH.Value)).ToList();
         }
+
+        public async Task<User> GetById(int id)
+        {
+            var user = await _redis.GetDatabase().HashGetAsync("user", id.ToString());
+            return JsonSerializer.Deserialize<User>(user);
+        }
+        public async Task<List<User>> GetRandomly(int count)
+        {
+            var users = await _redis.GetDatabase().HashRandomFieldsWithValuesAsync("user", count);
+            return users.Select(user => JsonSerializer.Deserialize<User>(user.Value)).ToList();
+        }
+
     }
 }
