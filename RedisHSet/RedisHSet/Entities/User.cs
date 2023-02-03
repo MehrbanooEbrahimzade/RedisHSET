69e7d1fbbd23035e81b4d4b486adcd9565d92469
mehrbanoo ebrahimzade <mehrbanooebrahimzade@gmail.com>
2023-02-03 21:32:33 +0330

    add user entity

--- RedisHSet/RedisHSet/Entities/User.cs
+++ RedisHSet/RedisHSet/Entities/User.cs
@@ -2,9 +2,15 @@
 {
     public class User
     {
+        public User(int id ,string userName, string email)
+        {
+            Id = id;
+            UserName = userName;
+            Email = email;
+        }
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
+        public string UserName { get;private set; }
+        public string Email { get;private set; }
 
     }
 }
